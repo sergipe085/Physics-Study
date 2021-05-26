f96158df6f802a2b5c723e23a6b12485884db323
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-26 09:08:58 -0300

    player initial setup

---
+++ Assets/Scripts/Stickman/Balance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balance : MonoBehaviour
+{
+    public float        targetRotation = 0.0f;
+    public Rigidbody2D  rig            = null;
+    public float        force          = 100.0f;
+
+    private void Start() {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        rig.MoveRotation(Mathf.LerpAngle(rig.rotation, targetRotation, force * Time.fixedDeltaTime));
+    }
+}
